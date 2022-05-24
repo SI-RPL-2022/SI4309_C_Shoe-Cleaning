8889344811c7e4a96f2471e63c23648ce61e6b91
foxxxesky <abdurrahman1467@gmail.com>
2022-05-24 13:58:40 +0800

    home page slicing #2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,15 +1,209 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.main')
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Shoe Cleaning</title>
-</head>
+@section('content')
+<!-- Jumbotron -->
+<section id="home">
+    <div id="jumbotron">
+        <!-- Alert Login Admin -->
+        @if(session()->has('loginAdmin'))
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            {{ session('loginAdmin') }}
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+        @endif
 
-<body>
+        <div class="container pt-5 pb-5">
+            <div class="row justify-content-around">
+                <div class="col-7">
+                    <div class="pt-5">
+                        <h4 id="text-jumbotron">Shoe Cleaning Siap Membersihkan Sepatu Anda!</h4>
+                        <p id="desc-jumbotron">Percayakan sepatu kesukaan anda kepada kami!</p>
+                    </div>
+                    <a id="button-jumbotron" class="btn btn-light" href="/Service">Bersihkan Sekarang!</a>
+                </div>
+                <div class="col-5">
+                    <img id="img-jumbotron" src="images/char.png" alt="">
+                </div>
+            </div>
+        </div>
+        <div class="pb-5"></div>
+        <div class="pb-3"></div>
+    </div>
+</section>
+<!-- Jumbotron -->
 
-</body>
+<!-- Layanan Kami -->
+<section id="service">
+    <h4 class="text-center pt-5" id="text-layanankami">Layanan Kami</h4>
+    <div class="text-center">
+        <img src="images/line.png" alt="">
+    </div>
+    <p class="text-center pt-2 pb-2" id="desc-layanan">Kami telah mencuci lebih dari 1000 pasang sepatu, dan akan
+        terus bertambah... <br>
+        Berikut jasa yang kami tawarkan</p>
+    <div class="container pb-5">
+        <div class="row justify-content-around">
+            <div class="col-3">
+                <img id="img-layanan" src="images/standart.png" alt="">
+                <h5 class="text-center pt-3" id="item-layanan">Standart Clean</h5>
+            </div>
+            <div class="col-3">
+                <img id="img-layanan" src="images/deep.png" alt="">
+                <h5 class="text-center pt-3" id="item-layanan">Deep Clean</h5>
+            </div>
+            <div class="col-3">
+                <img id="img-layanan" src="images/premium.png" alt="">
+                <h5 class="text-center pt-3" id="item-layanan">Premium Clean</h5>
+            </div>
+            <div class="col-3">
+                <img id="img-layanan" src="images/weekend.png" alt="">
+                <h5 class="text-center pt-3" id="item-layanan">Weekend Clean</h5>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Layanan Kami -->
 
-</html>
+<!-- Bagaimana Kami Bekerja -->
+<section>
+    <div class="container pb-5">
+        <h4 class="text-center pt-5" id="text-carakerja">Bagaimana <span id="slip-titlecarakerja">Kami</span> Bekerja
+        </h4>
+        <div class="text-center">
+            <img src="images/line.png" alt="">
+        </div>
+        <p class="text-center pt-2 pb-2" id="desc-carakerja">Simpan uang dan waktu, dengan gratis jemput di rumah atau
+            kantor anda
+        </p>
+        <div class="row justify-content-around">
+            <div class="col-4">
+                <div class="text-center">
+                    <img id="img-carakerja" src="images/order.png" alt="">
+                </div>
+                <p class="text-center pt-3" id="item-carakerja"><span id="slip-color-carakerja">Order - </span>Choose
+                    our<br>service then order</p>
+            </div>
+            <div class="col-4">
+                <div class="text-center">
+                    <img id="img-carakerja" src="images/pickup.png" alt="">
+                </div>
+                <p class="text-center pt-3" id="item-carakerja"><span id="slip-color-carakerja">We Pickup - </span>The
+                    driver<br>picks up your shoes</p>
+            </div>
+            <div class="col-4">
+                <div class="text-center">
+                    <img id="img-carakerja" src="images/deliver.png" alt="">
+                </div>
+                <p class="text-center pt-3" id="item-carakerja"><span id="slip-color-carakerja">We Deliver - </span>Your
+                    shoes are<br>finished, ready to wear
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Bagaimana Kami Bekerja -->
+
+<!-- Testimoni -->
+<section id="testi">
+    <h4 class="text-center pt-5" id="text-testimoni">Testimoni</h4>
+    <div class="text-center">
+        <img src="images/line.png" alt="">
+    </div>
+    <div class="container pt-3 pb-5">
+        <div class="row justify-content-around">
+            <div class="col-4">
+                <img id="img-testimoni" src="images/testimoni.png" alt="">
+            </div>
+            <div class="col-6 pt-5">
+                <h4 id="text-title-testimoni">Pelanggan Kami Mengatakan</h4>
+                <p id="desc-testimoni">“Website pembersih sepatu membantu saya dalam aktivitas sehari-hari, bahkan saya
+                    dapat melakukan
+                    aktivitas dengan sangat cepat, terima kasih pembersih sepatu”</p>
+                <h4 class="pt-2" id="person">Felicia</h4>
+                <p id="location">Bojongsoang, Bandung</p>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Testimoni -->
+
+<!-- Tentang Kami -->
+<section id="about">
+    <div class="container pt-5 pb-5">
+        <h4 class="text-center" id="text-tentang">Tentang Kami</h4>
+        <div class="text-center pb-3">
+            <img src="images/line.png" alt="">
+        </div>
+        <p class="text-center" id="desc-tentang">Berawal dari mata kuliah Web Application Development, Manajemen Rantai
+            Pasok dan masalah yang ada. ShoeCleaning bertujuan untuk membantu mempermudah aktivitas orang-orang setiap
+            harinya. shoecleaning didirikan oleh 6 mahasiswa jenius syarif, hadi, ryan, mubeth, harun dan icha yang
+            mempunyai
+            motivasi tinggi untuk membuat perubahan disekitarnya, berharap inovasi yang dibuat dapat membantu orang
+            banyak. sehingga kedepannya dapat memotivasi mahasiswa lainnya untuk menciptakan inovasi yang lebih baik
+            lagi.</p>
+
+        <p class="text-center pt-2" id="kisahkami">Baca Kisah Kami</p>
+    </div>
+</section>
+<!-- Tentang Kami -->
+
+<!-- Our Team -->
+<section>
+    <div class="container pt-5 pb-5">
+        <h4 class="text-center" id="text-team">Tim Kami</h4>
+        <div class="text-center pb-5">
+            <img src="images/line.png" alt="">
+        </div>
+        <!-- Row 1 -->
+        <div class="row justify-content-around">
+            <div class="col-4">
+                <div class="text-center pb-3">
+                    <img id="img-team" src="images/ryan.png" alt="">
+                </div>
+                <h4 id="name" class="text-center">Ryandi Yorsinda Anggoro</h4>
+                <p id="role" class="text-center">UI/UX Designer</p>
+            </div>
+            <div class="col-4">
+                <div class="text-center pb-3">
+                    <img id="img-team" src="images/syarif.png" alt="">
+                </div>
+                <h4 id="name" class="text-center">Syariif Abdurrahman Bathik</h4>
+                <p id="role" class="text-center">Project Manager</p>
+            </div>
+            <div class="col-4">
+                <div class="text-center pb-3">
+                    <img id="img-team" src="images/mubeth.png" alt="">
+                </div>
+                <h4 id="name" class="text-center">Mubeth Praditya</h4>
+                <p id="role" class="text-center">Web Developer</p>
+            </div>
+        </div>
+
+        <!-- Row 2 -->
+        <div class="row justify-content-around pt-5">
+            <div class="col-4">
+                <div class="text-center pb-3">
+                    <img id="img-team" src="images/ica.png" alt="">
+                </div>
+                <h4 id="name" class="text-center">Luh Putu Kartika Yasa Putri</h4>
+                <p id="role" class="text-center">UX Researcher</p>
+            </div>
+            <div class="col-4">
+                <div class="text-center pb-3">
+                    <img id="img-team" src="images/hadi.png" alt="">
+                </div>
+                <h4 id="name" class="text-center">Hadi Noerddien Jayanto</h4>
+                <p id="role" class="text-center">Data Engineer</p>
+            </div>
+            <div class="col-4">
+                <div class="text-center pb-3">
+                    <img id="img-team" src="images/harun.png" alt="">
+                </div>
+                <h4 id="name" class="text-center">Hasrinaldi Hasniman Harun</h4>
+                <p id="role" class="text-center">System Analyst</p>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Our Team -->
+@endsection
