135897f15b7e5546a6580e4b27e372b4679dac32
foxxxesky <abdurrahman1467@gmail.com>
2022-05-24 20:41:42 +0800

    fix order post route #5

--- routes/web.php
+++ routes/web.php
@@ -34,4 +34,4 @@ Route::get('/HomeAdmin', [Authentication::class, 'indexAdmin'])->middleware('is_
 // Service User
 Route::get('/Service', [Customer::class, 'service']);
 Route::get('/Order', [Customer::class, 'order'])->middleware('auth');
-Route::post('/Order', [MainController::class, 'store'])->middleware('auth')->name('checkouts');
+Route::post('/Order', [Customer::class, 'store'])->middleware('auth')->name('checkouts');
