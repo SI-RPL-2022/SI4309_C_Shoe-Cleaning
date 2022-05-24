49d80ff3ad83cb417c177f4a2e6b20d661b64a15
foxxxesky <abdurrahman1467@gmail.com>
2022-05-24 20:18:49 +0800

    add service view method #5

--- app/Http/Controllers/Customer.php
+++ app/Http/Controllers/Customer.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class Customer extends Controller
 {
-    //
+    public function service()
+    {
+        return view('pages.user.service', ['pages' => 'Service']);
+    }
 }
