413b10b51b51fdadf7f7b79582f69937a81f7348
foxxxesky <abdurrahman1467@gmail.com>
2022-05-24 14:09:10 +0800

    add user register method #2

--- app/Http/Controllers/Authentication.php
+++ app/Http/Controllers/Authentication.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class Authentication extends Controller
@@ -14,5 +15,22 @@ class Authentication extends Controller
     public function registerPage()
     {
         return view('pages.login&register.register');
+    }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'email' => 'required|email:dns|unique:users',
+            'no_hp' => 'required',
+            'password' => 'required|confirmed',
+            'setujukebijakan' => 'accepted'
+        ]);
+
+        $validatedData['password'] = Hash::make($validatedData['password']);
+
+        User::create($validatedData);
+        return redirect('/Login')->with('success', 'Registrasi Berhasil Silahkan Login');
     }
 }
