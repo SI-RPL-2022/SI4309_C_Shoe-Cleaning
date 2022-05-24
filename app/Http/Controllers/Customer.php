6694feb6ccba907cba95f371ffdccf013c54d45f
foxxxesky <abdurrahman1467@gmail.com>
2022-05-24 20:39:29 +0800

    add order store method #5

--- app/Http/Controllers/Customer.php
+++ app/Http/Controllers/Customer.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -32,5 +33,27 @@ class Customer extends Controller
         }
         
         return view('pages.user.order', ['data' => $user], ['price' => $total, 'pages' => 'Checkouts']);
+    }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'nama_produk' => 'required',
+            'user_id' => 'required',
+            'jumlah' => 'required',
+            'harga' => 'required',
+            'nama_pemesan' => 'required',
+            'no_hp' => 'required',
+            'tanggal_pickup' => 'required',
+            'alamat_penjemputan' => 'required',
+            'alamat_pengiriman' => 'required',
+            'alamat_pengiriman' => 'required',
+            'pembayaran' => 'required|in:LinkAja,OVO,Gopay,COD',
+            'harga' => 'required',
+            'catatan' => 'nullable'
+        ]);
+
+        Order::create($validatedData);
+        return redirect('/OrderSaya')->with('success', 'Order Berhasil!');
     }
 }
