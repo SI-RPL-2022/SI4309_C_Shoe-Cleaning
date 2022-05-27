fea73b33d3172107c4ee0c469d14e48bb6b108f0
foxxxesky <abdurrahman1467@gmail.com>
2022-05-27 22:48:33 +0800

    create add product page

--- resources/views/pages/admin/addproduct.blade.php
+++ resources/views/pages/admin/addproduct.blade.php
@@ -1,5 +1,34 @@
 @extends('layouts.mainadmin')
 
 @section('content')
-<h2>add product</h2>
+<div class="container">
+    <div class="row pt-5 pb-5">
+        <div class="col">
+            <h3 id="product-heading">Buat Produk</h3>
+        </div>
+    </div>
+
+    <form action="" method="post">
+        @csrf
+        <div class="mb-3">
+            <label class="form-label">Foto Produk</label>
+            <input class="form-control" type="file">
+        </div>
+
+        <div class="mb-3">
+            <label class="form-label">Nama Produk</label>
+            <input type="text" class="form-control">
+        </div>
+
+        <div class="mb-3">
+            <label class="form-label">Harga Produk</label>
+            <input type="text" class="form-control">
+        </div>
+
+        <div class="mb-3">
+            <label class="form-label">Example textarea</label>
+            <textarea class="form-control" rows="3"></textarea>
+        </div>
+    </form>
+</div>
 @endsection
