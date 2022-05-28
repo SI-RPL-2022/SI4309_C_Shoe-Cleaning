98633e175c1fd0c8710cdbfafcbdf3a976e0c7d3
foxxxesky <abdurrahman1467@gmail.com>
2022-05-29 06:15:39 +0800

    fix service & chechouts page done #7

--- app/Http/Controllers/Customer.php
+++ app/Http/Controllers/Customer.php
@@ -4,33 +4,23 @@ namespace App\Http\Controllers;
 
 use App\Models\Order;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 
 class Customer extends Controller
 {
     public function service()
     {
-        return view('pages.user.service', ['pages' => 'Service']);
+        $products = DB::table('products')->get();
+        return view('pages.user.service', ['pages' => 'Service'], compact('products'));
     }
 
     public function order(Request $request)
     {
         $user = Auth::user();
-
-        $service = $request->get('service');
-        if ($service === 'Standart Clean') {
-            $count = $request->get('jumlah');
-            $total = $count * 30000;
-        } elseif ($service === 'Deep Clean') {
-            $count = $request->get('jumlah');
-            $total = $count * 40000;
-        } elseif ($service === 'Premium Clean') {
-            $count = $request->get('jumlah');
-            $total = $count * 45000;
-        } elseif ($service === 'Weekend Clean') {
-            $count = $request->get('jumlah');
-            $total = $count * 50000;
-        }
+        $count = $request->get('jumlah');
+        $harga = $request->get('harga');
+        $total = $count * $harga;
         
         return view('pages.user.order', ['data' => $user], ['price' => $total, 'pages' => 'Checkouts']);
     }
