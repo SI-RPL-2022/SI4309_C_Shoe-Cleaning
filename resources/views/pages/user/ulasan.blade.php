9fdf2ba9a04d27c17b898f6e2cb68bb6f96a62f7
foxxxesky <abdurrahman1467@gmail.com>
2022-06-17 17:32:48 +0800

    slicing ulasan page done #13

--- resources/views/pages/user/ulasan.blade.php
+++ resources/views/pages/user/ulasan.blade.php
@@ -2,18 +2,26 @@
 
 @section('content')
 <section>
-    <div class="container pb-4">
-        <h3 class="fw-bold pb-3">Ulasan Produk</h3>
-        <p>Berikan ulasan untuk produk yang sudah anda pesan</p>
+    <div class="container pb-4 pt-5">
+        <h3 class="fw-bold pb-2" id="ulasan-heading">Ulasan Produk</h3>
+        <p id="ulasan-text">Berikan ulasan untuk produk yang sudah anda pesan</p>
     </div>
 
-    <div class="container">
+    <div class="container pb-5">
         <div class="row">
-            <div class="col-3">
-                <h3>{{ $data->nama_produk }}</h3>
-                <p>{{ $desc }}</p>
+            <div class="col-4">
+                <h3 id="ulasan-subheading">{{ $data->nama_produk }}</h3>
+                <p id="ulasan-text">{{ $desc }}</p>
             </div>
-            <div class="col-9"></div>
+            <div class="col-8">
+                <form action="/Ulasan" method="post">
+                    @csrf
+                    <textarea class="w-100" name="ulasan" cols="50" rows="10"></textarea>
+                    <div class="text-end pt-3">
+                        <button class="btn text-white" id="ulasan-button" type="submit">Ok</button>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
 </section>
