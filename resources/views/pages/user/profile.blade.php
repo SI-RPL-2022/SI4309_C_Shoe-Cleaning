ce4da66e8ec9db7e2a28fea5898022c98ddb7819
hadimasdi <hadimasdi@gmail.com>
2022-06-21 09:16:57 +0700

    Update Profile Page #12

---
+++ resources/views/pages/user/profile.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.main')
+
+@section('content')
+<section>
+    <div class="container pt-5 pb-5">
+        <div class="row justify-content-center">
+            <div class="col-8">
+                <h4 id="profile-title" class="pb-2">Profile</h4>
+                <hr style="color: #504a4a; height: 2px;">
+               
+                <hr style="color: #504a4a; height: 2px;">
+            </div>
+        </div>
+    </div>
+</section>
+@endsection
