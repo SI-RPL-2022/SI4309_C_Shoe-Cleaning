9c098b41f69b01225a9148bed8782d87ea198835
hadimasdi <hadimasdi@gmail.com>
2022-06-21 09:41:29 +0700

    Menambahkan Route Finish Detail #11

--- routes/web.php
+++ routes/web.php
@@ -43,6 +43,7 @@ Route::post('/addProduct', [Admin::class, 'storeProduct'])->middleware('is_admin
 Route::get('/OrderDetail', [Admin::class, 'orderDetail'])->middleware('is_admin');
 Route::post('/UpdateOrder/{id}', [Admin::class, 'updateOrder'])->name('updateOrder')->middleware('is_admin');
 Route::get('/DataSelesai', [Admin::class, 'selesai'])->middleware('is_admin');
+Route::get('/FinishDetail', [MainController::class, 'finishDetail'])->middleware('is_admin');
 
 // Service User
 Route::get('/Service', [Customer::class, 'service']);
