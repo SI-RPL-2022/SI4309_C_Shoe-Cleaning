167f93a1e6cab403988bb9f50952be026ebb9825
hadimasdi <hadimasdi@gmail.com>
2022-06-21 09:32:43 +0700

    Menambahkan tampilan Finish View #11

---
+++ resources/views/pages/admin/finish.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.mainadmin')
+
+@section('content')
+<section>
+    <div class="container pt-5 pb-5">
+        <h4 class="pb-3" id="welcome">Pesanan Selesai</h4>
+        <div class="row justify-content-around">
+            <div class="col">
+            </div>
+        </div>
+    </div>
+</section>
+@endsection
